aacb2446a7c35b9a56378a40fe52660187ae8ec1
jackwu <1683358846@qq.com>
2017-08-05 19:36:22 +0000

    modify it

--- inc/rb_tree_type.h
+++ inc/rb_tree_type.h
@@ -21,6 +21,7 @@ typedef void (*handler)(int);
 typedef struct __node_task{
  int tid;
  int value;
+
  RB_ENTRY(__node_task)entry;
 }task_t;
 
@@ -34,8 +35,9 @@ RB_HEAD(task_head,__node_task)head=RB_INITIALIZER(NULL);
 
 
 /*comp a and b*/
+/*note:com must return >0value and <0 value*/
 int cmp(task_t *a,task_t *b){
-      return a->value>b->value?a->value:b->value;
+      return a->value>b->value?1:-1;
 }
 
 void append(task_t *a){
--- src/main.c
+++ src/main.c
@@ -3,19 +3,16 @@
 #include <stdlib.h>
 #include "../inc/tree.h"
 
-
-
 int main(int arc,char **argv){
     
      task_t a,b;
      a.value=2;
      b.value=1;
-     
+
      append(&a);
      append(&b);
      
      print();
-
- 
+  
     return 0;
 }
