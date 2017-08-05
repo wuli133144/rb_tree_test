cf8a660628475ca6969fbdf0b3f2119a78867abc
jackwu <1683358846@qq.com>
2017-08-05 19:28:19 +0000

    test main

---
+++ src/main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../inc/tree.h"
+
+
+
+int main(int arc,char **argv){
+    
+     task_t a,b;
+     a.value=2;
+     b.value=1;
+     
+     append(&a);
+     append(&b);
+     
+     print();
+
+ 
+    return 0;
+}
